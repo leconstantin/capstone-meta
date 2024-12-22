b06e779753bf880e278e6225518a88f1494055b6
leconstantin <lcon69184@gmail.com>
2024-12-22 19:20:43 +0200

    added last change

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
     <main>
-      <h1 className="text-3xl font-bold underline"> Home page</h1>
+      <h1 className="text-3xl font-bold underline"> Home page now</h1>
     </main>
   );
 }
